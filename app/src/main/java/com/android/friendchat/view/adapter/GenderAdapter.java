011896a144d7521b64a6489810438bf4939e1144
degea9 <degea9@gmail.com>
2016-10-06 14:17:51 +0700

    Edit profile screen

---
+++ app/src/main/java/com/android/friendchat/view/adapter/GenderAdapter.java
@@ -0,0 +1,56 @@
+package com.android.friendchat.view.adapter;
+
+import com.android.friendchat.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by hp 400 on 10/6/2016.
+ */
+public class GenderAdapter extends BaseAdapter {
+    Context context;
+    int icons[];
+    String[] genders;
+    LayoutInflater inflter;
+    ImageView icon;
+    TextView gender;
+
+    public GenderAdapter(Context applicationContext, int[] flags, String[] genders) {
+        this.context = applicationContext;
+        this.icons = flags;
+        this.genders = genders;
+        inflter = (LayoutInflater.from(applicationContext));
+    }
+
+    @Override
+    public int getCount() {
+        return icons.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflter.inflate(R.layout.gender_spinner_item, null);
+        ButterKnife.bind(view);
+        icon.setImageResource(icons[i]);
+        gender.setText(genders[i]);
+        return view;
+    }
+}
--- app/src/main/java/com/android/friendchat/view/fragment/EditProfileFragment.java
+++ app/src/main/java/com/android/friendchat/view/fragment/EditProfileFragment.java
@@ -6,14 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import com.android.friendchat.R;
+import com.android.friendchat.view.adapter.GenderAdapter;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EditProfileFragment extends BaseFragment {
-
+    @Bind(R.id.profile_gender)
+    Spinner mGender;
 
     public EditProfileFragment() {
         // Required empty public constructor
@@ -24,7 +30,17 @@ public class EditProfileFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        View view =  inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        ButterKnife.bind(view);
+        setUpSpinner();
+        return view;
+    }
+
+    private void setUpSpinner() {
+        int[] flags = {R.drawable.ic_male,R.drawable.ic_female};
+        String[] genders = getResources().getStringArray(R.array.genders);
+        GenderAdapter genderAdapter=new GenderAdapter(getActivity().getApplicationContext(),flags,genders);
+        mGender.setAdapter(genderAdapter);
     }
 
 }
