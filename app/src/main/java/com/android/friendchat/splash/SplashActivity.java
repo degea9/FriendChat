105a18d05f6e3ccfc7d23d56d9cd20e1ee1e63e7
tuandang <degea9@gmail.com>
2016-10-17 23:44:38 +0700

    firebase presence

--- app/src/main/java/com/android/friendchat/splash/SplashActivity.java
+++ app/src/main/java/com/android/friendchat/splash/SplashActivity.java
@@ -6,6 +6,12 @@ import com.android.friendchat.R;
 import com.android.friendchat.base.BaseActivity;
 import com.android.friendchat.main.RoomActivity;
 import com.android.friendchat.signin.LoginActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
 
 public class SplashActivity extends BaseActivity implements SplashView {
     private SplashPresenter mPresenter;
@@ -14,20 +20,56 @@ public class SplashActivity extends BaseActivity implements SplashView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        mPresenter = new SplashPresenter(this);
+        //mPresenter = new SplashPresenter(this);
+        testPresense();
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        mPresenter.onStart();
+        //mPresenter.onStart();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        mPresenter.onStop();
+        //mPresenter.onStop();
 
+    }
+
+    private void testPresense() {
+        // since I can connect from multiple devices, we store each connection instance separately
+        // any time that connectionsRef's value is null (i.e. has no children) I am offline
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myConnectionsRef = database.getReference("users/joe/connections");
+
+        // stores the timestamp of my last disconnect (the last time I was seen online)
+        final DatabaseReference lastOnlineRef = database.getReference("/users/joe/lastOnline");
+
+        final DatabaseReference connectedRef = database.getReference(".info/connected");
+        connectedRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                boolean connected = snapshot.getValue(Boolean.class);
+                if (connected) {
+                    // add this device to my connections list
+                    // this value could contain info about the device or a timestamp too
+                    DatabaseReference con = myConnectionsRef.push();
+                    con.setValue(Boolean.TRUE);
+
+                    // when this device disconnects, remove it
+                    con.onDisconnect().removeValue();
+
+                    // when I disconnect, update the last time I was seen online
+                    lastOnlineRef.onDisconnect().setValue(ServerValue.TIMESTAMP);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                System.err.println("Listener was cancelled at .info/connected");
+            }
+        });
     }
 
     @Override
